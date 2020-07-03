cbb351d02e27501696d86cfd1f31d6813e8e3d57
Elmo Yeldo <elmoyeldo@gmail.com>
2020-07-03 02:13:24 +0100

    Initial Commit

---
+++ src/main/java/com/logicalsapien/ukrem/UkremApplication.java
@@ -0,0 +1,13 @@
+package com.logicalsapien.ukrem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UkremApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UkremApplication.class, args);
+	}
+
+}

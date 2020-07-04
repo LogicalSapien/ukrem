0b41914bf784f1bb1afae35064f00ac6cdbb87db
Elmo Yeldo <elmoyeldo@gmail.com>
2020-07-04 22:38:47 +0100

    Add to sonarcloud

---
+++ src/test/java/com/logicalsapien/ukrem/controller/SampleControllerTest.java
@@ -0,0 +1,48 @@
+package com.logicalsapien.ukrem.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.ResponseEntity;
+
+import java.net.URL;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+
+/**
+ * Sample Controller Tests.
+ */
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class SampleControllerTest {
+
+  /**
+   * To bind the Random Port.
+   */
+  @LocalServerPort
+  private int port;
+
+
+  /**
+   * Rest Template for testing the api.
+   */
+  @Autowired
+  private TestRestTemplate restTemplate;
+
+  /**
+   * List all Planets test.
+   * @throws Exception exception
+   */
+  @Test
+  public void listAllPlanetsTest() throws Exception {
+
+    ResponseEntity<String> response = restTemplate.getForEntity(
+            new URL("http://localhost:" + port + "/api/sample").toString(), String.class);
+
+    assertThat(response.getBody(), equalTo("It Works..."));
+  }
+
+}

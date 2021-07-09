8960ae8251d20688b98beec92a74af17e8ce0d98
Elmo Yeldo <elmoyeldo@gmail.com>
2021-07-09 11:02:04 +0100

    checkstyle fixes

--- src/main/java/com/logicalsapien/ukrem/UkremApplication.java
+++ src/main/java/com/logicalsapien/ukrem/UkremApplication.java
@@ -9,12 +9,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class UkremApplication {
 
-	/**
-	 * UkremApplication main method.
-	 * @param args args
-	 */
-	public static void main(final String[] args) {
-		SpringApplication.run(UkremApplication.class, args);
-	}
+  /**
+   * UkremApplication main method.
+   * @param args args
+   */
+  public static void main(final String[] args) {
+    SpringApplication.run(UkremApplication.class, args);
+  }
 
 }
